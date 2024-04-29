656beb3c0ef9ceae429b845bd4600aaec6b43dae
Anuroop <ksanu1998@gmail.com>
2024-04-28 18:16:21 -0700

    Update `plots`

--- scripts/plots/barplot_multimodal.py
+++ scripts/plots/barplot_multimodal.py
@@ -2,33 +2,62 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Define the metrics and model groups
-metrics_labels = ['Train Accuracy', 'Train Loss', 'Test Accuracy', 'Test Loss']
+metrics_labels = ['Train Accuracy', 'Test Accuracy']
 group_labels = ['2D CNN', '3D CNN', 'Transformer']
 
 # Specific encoders for each type
 encoders = [
     ['ResNet18', 'GoogLeNet', 'VGG16'],
+    # ['Simple3DCNN', 'I3D', 'Ablated I3D'],
     ['Simple3DCNN', 'I3D'],
-    ['ViT', 'TimeSformer']
+    ['ViT', 'VideoMAE']
 ]
 
-# Simulated data for each encoder, each with: [Train Accuracy, Train Loss, Test Accuracy, Test Loss]
+# Simulated data for each encoder, each with: [Train Accuracy, Test Accuracy]
+
+# Audio
+# data = {
+#     'ResNet18': [0.8216, 0.5825],
+#     'GoogLeNet': [0, 0],# Placeholder values
+#     'VGG16': [0.6366, 0.5120],
+#     'Simple3DCNN': [0.519, 0.514],
+#     'I3D': [0.623, 0.605],
+#     'Ablated I3D' : [0, 0.448],
+#     'ViT': [0.1634, 0.1738],
+#     'VideoMAE': [0, 0]  # Placeholder values
+# }
+
+# Vision
+# data = {
+#     'ResNet18': [0.8634, 0.6225],
+#     'GoogLeNet': [0, 0],# Placeholder values
+#     'VGG16': [0.9495, 0.7040],
+#     'Simple3DCNN': [0.546, 0.462],
+#     'I3D': [0.878, 0.831],
+#     'Ablated I3D' : [0, 0.540],
+#     'ViT': [0.8361, 0.5934],
+#     'VideoMAE': [0, 0]  # Placeholder values
+# }
+
+# Multimodal
 data = {
-    'ResNet18': [0.7, 0.8, 0.6, 0.9],
-    'GoogLeNet': [0.72, 0.85, 0.65, 0.88],
-    'VGG16': [0.75, 0.9, 0.68, 0.93],
-    'Simple3DCNN': [0.519, 1.213, 0.514, 1.294],
-    'I3D': [0.623, 0.991, 0.605, 1.022],
-    'ViT': [0.1634, 1.7931, 0.1738, 1.7908],
-    'TimeSformer': [0.2, 1.7, 0.19, 1.7]  # Placeholder values
+    'ResNet18': [0.8854, 0.6350],
+    'GoogLeNet': [0.925, 0.661],
+    'VGG16': [0.4329, 0.4716],
+    'Simple3DCNN': [0.647, 0.573],
+    'I3D': [0.923, 0.806],
+    # 'Ablated I3D' : [0, 0.540],
+    'ViT': [0.6811, 0.5598],
+    'VideoMAE': [0.334, 0.366]
 }
 
 # colors = ['#FF9999', '#99CCFF', '#99FF99', '#FFCC99', '#FF99CC', '#CCFF99', '#99FFFF']  # Colors for each encoder
-colors = ['red', 'green', 'blue', 'cyan', 'orange', 'yellow', 'purple']  # Colors for each encoder
+# colors = ['red', 'green', 'blue', 'cyan', 'orange', 'yellow', 'purple', 'black']  # Colors for each encoder
+colors = ['red', 'green', 'blue', 'cyan', 'orange', 'purple', 'black']  # Colors for each encoder
 # hatches = ['/', '\\', 'o', 'x']  # Different hatches for each metric
 
 # Plotting
-fig, ax = plt.subplots(figsize=(14, 8))
+fig, ax = plt.subplots(figsize=(6, 6))
 
 metric_group_width = 0.8
 bar_width = metric_group_width / (len(encoders[0]) + len(encoders[1]) + len(encoders[2]) + 1)
@@ -58,11 +87,15 @@ for i, metric in enumerate(metrics_labels):
         offset += bar_width * 0.5  # Add spacer after each group
     offset += bar_width * 1.5  # Extra space between metrics
 
+
+ax.set_ylim(0.0, 1.0)
+ax.set_yticks(np.arange(0.0, 1.1, 0.1))
 # Add some text for labels, title and custom x-axis tick labels, etc.
 # ax.set_xlabel('Metrics')
 ax.set_ylabel('Values', fontsize=12)
-ax.set_title('Unimodal audio pipeline for different encoders')
-ax.set_xticks(x + (metric_group_width + 2.5 * bar_width) / 2)
+# ax.set_title('Unimodal visual pipeline for different encoders')
+ax.set_title('Multimodal for different encoders')
+ax.set_xticks(x + (metric_group_width + 0.0 * bar_width) / 2)
 ax.tick_params(axis='x', which='both', bottom=False)  # Remove x-tick markers
 ax.set_xticklabels(metrics_labels, y=-0.05, fontsize=12)
 # ax.legend([plt.Rectangle((0,0),1,1, color='gray', hatch=hatch) for hatch in hatches], metrics_labels, title='Metric')
@@ -74,7 +107,8 @@ for i in range(len(metrics_labels) - 1):
     ax.axvline(x[i] + (len(encoders[2]) + 6.5) * bar_width, color='black', linestyle='-', linewidth=2)
 
 
-ax.axhline(1.0, color='red', linestyle='-', linewidth=2)
+# ax.axhline(1.0, color='red', linestyle='-', linewidth=2)
 
 fig.tight_layout()
+plt.savefig('multimodal.png')
 plt.show()
